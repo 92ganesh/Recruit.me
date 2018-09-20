c4276927ec8455d3302e5a96d4c697377409f2d6
shubham muniyal <shubhammuniyal@gmail.com>
2018-09-20 15:48:54 +0530

    Indented code

--- integratedProject/WP/src/SendEmail.java
+++ integratedProject/WP/src/SendEmail.java
@@ -12,15 +12,14 @@ import javax.mail.*;
 import javax.mail.internet.*; 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import javax.activation.FileDataSource;   
+import javax.activation.FileDataSource;
 
 
 public class SendEmail{    
 	 public static void main(String[] args) {   
-		   String[] recipients = {"tp92ganeshpatra@gmail.com"};	// enter valid emails only as it does not check if email exists of not
+		   String[] recipients = {"shubhammuniyal@gmail.com"};	// enter valid emails only as it does not check if email exists of not
 		   SendEmail.send("123tmails@gmail.com", "12345pass",	recipients,	"Not so urgent", "text message");  // enter your details
 		   SendEmail.sendWithAttachment("123tmails@gmail.com", "12345pass",	recipients,	"This is subject part", "text message","E:\\interestingImages\\natureSea.jpeg");
-
 	 }    
 	 
 	 /** details- sends mail using gmail SMTP server and javaMail API 
@@ -106,8 +105,8 @@ public class SendEmail{
 		      	    } while (ex != null);
 	          }                 
 	    }
-		
-		/** details- sends mail (which consists of text and attachment) using gmail SMTP server and javaMail API 
+	    
+	    /** details- sends mail (which consists of text and attachment) using gmail SMTP server and javaMail API 
 	  	* params:- 
 	    * from 		- sender's email 
 	    * password	- sender's password
