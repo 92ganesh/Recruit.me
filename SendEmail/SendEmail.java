bc0f17d409dea31db68f560b12bf4c95c379a2ed
Ganesh Patra <ganesh.patra@somaiya.edu>
2019-10-10 08:47:16 +0000

    Update SendEmail.java

--- SendEmail/SendEmail.java
+++ SendEmail/SendEmail.java
@@ -18,8 +18,8 @@ import javax.activation.FileDataSource;
 public class SendEmail{    
 	 public static void main(String[] args) {   
 		   String[] recipients = {"sample@gmail.com"};	// enter valid emails only as it does not check if email exists of not
-		   SendEmail.send("123tmails@gmail.com", "12345pass",	recipients,	"Not so urgent", "text message");  // enter your details
-		   SendEmail.sendWithAttachment("123tmails@gmail.com", "12345pass",	recipients,	"This is subject part", "text message","E:\\interestingImages\\natureSea.jpeg");
+		   SendEmail.send("your email", "your password",	recipients,	"Not so urgent", "text message");  // enter your details
+		   SendEmail.sendWithAttachment("your email", "your password",	recipients,	"This is subject part", "text message","E:\\interestingImages\\natureSea.jpeg");
 	 }    
 	 
 	 /** details- sends mail using gmail SMTP server and javaMail API 
